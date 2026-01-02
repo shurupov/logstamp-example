1ecc2ce6501e0305994c0f00eaa27db037455cf1
shurupov <bolteg86@ya.ru>
2026-01-03 03:01:41 +0400

    Checked additional MDC parameters

--- service-one/src/main/java/com/logging/presentation/service_one/controller/ClaimController.java
+++ service-one/src/main/java/com/logging/presentation/service_one/controller/ClaimController.java
@@ -3,6 +3,7 @@ package com.logging.presentation.service_one.controller;
 import com.logging.presentation.service_one.controller.request.StartClaimRequest;
 import com.logging.presentation.service_one.controller.response.ClaimIdResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.jboss.logging.MDC;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,8 +16,11 @@ public class ClaimController {
 
     @PostMapping("/claims")
     public ClaimIdResponse start(@RequestBody StartClaimRequest startClaimRequest) {
+        UUID claimId = UUID.randomUUID();
+        MDC.put("claimId", claimId.toString());
         log.info("Получено {}", startClaimRequest);
         log.error("Исключение", new RuntimeException("Что-то пошло не так"));
-        return new ClaimIdResponse(UUID.randomUUID());
+        MDC.remove("claimId");
+        return new ClaimIdResponse(claimId);
     }
 }
